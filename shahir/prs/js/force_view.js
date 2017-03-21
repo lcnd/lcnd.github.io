06f71b7b76fe6d606643a02ba67d5279c2e677c6
lcnd <cognitive.neurodynamics@gmail.com>
2017-03-21 08:06:39 -0400

    update

--- shahir/prs/js/force_view.js
+++ shahir/prs/js/force_view.js
@@ -1,148 +1,149 @@
-var width = 285,
-    height = 285,
-    fill = d3.scale.category20();
+var force_view_js = function() {
+  var width = 285,
+      height = 285,
+      fill = d3.scale.category20();
 
-// mouse event vars
-var selected_node = null,
-    selected_link = null,
-    mousedown_link = null,
-    mousedown_node = null,
-    mouseup_node = null;
+  // mouse event vars
+  var selected_node = null,
+      selected_link = null,
+      mousedown_link = null,
+      mousedown_node = null,
+      mouseup_node = null;
 
-// init svg
-var outer = d3.select("#chart1")
-            .append("svg:svg")
-            .attr("width", width)
-            .attr("height", height)
-            .attr("pointer-events", "all");
+  // init svg
+  var outer = d3.select("#chart1")
+              .append("svg:svg")
+              .attr("width", width)
+              .attr("height", height)
+              .attr("pointer-events", "all");
 
-var vis = outer
-  .append('svg:g')
-    .call(d3.behavior.zoom().on("zoom", rescale))
-    .on("dblclick.zoom", null)
-  .append('svg:g')
-    .on("mousemove", mousemove)
-    .on("mousedown", mousedown)
-    .on("mouseup", mouseup);
+  var vis = outer
+            .append('svg:g')
+            .call(d3.behavior.zoom().on("zoom", rescale))
+            .on("dblclick.zoom", null)
+            .append('svg:g')
+            .on("mousemove", mousemove)
+            .on("mousedown", mousedown)
+            .on("mouseup", mouseup);
 
-vis.append('svg:rect')
-    .attr('width', width)
-    .attr('height', height)
-    .attr('fill', 'transparent');
+  vis.append('svg:rect')
+  .attr('width', width)
+  .attr('height', height)
+  .attr('fill', 'transparent');
 
-// init force layout
-var force = d3.layout.force()
-    .size([width, height])
-.links([{source: 0, target: 1},{source: 0, target: 2},{source: 0, target: 3},{source: 0, target: 4},{source: 0, target: 5},{source: 0, target: 6},{source: 0, target: 7},{source: 7, target: 8},{source: 7, target: 9},{source: 7, target: 10},{source: 7, target: 11},{source: 8, target: 9},{source: 9, target: 10},{source: 10, target: 11},{source: 11, target: 8}])
-.nodes([{},{},{},{},{},{},{},{},{},{},{},{}])
-    .linkDistance(50)
-    .charge(-200)
-    .on("tick", tick);
+  // init force layout
+  var force = d3.layout.force()
+              .size([width, height])
+              .links([{source: 0, target: 1},{source: 0, target: 2},{source: 0, target: 3},{source: 0, target: 4},{source: 0, target: 5},{source: 0, target: 6},{source: 0, target: 7},{source: 7, target: 8},{source: 7, target: 9},{source: 7, target: 10},{source: 7, target: 11},{source: 8, target: 9},{source: 9, target: 10},{source: 10, target: 11},{source: 11, target: 8}])
+              .nodes([{},{},{},{},{},{},{},{},{},{},{},{}])
+              .linkDistance(50)
+              .charge(-200)
+              .on("tick", tick);
 
 
-// line displayed when dragging new nodes
-var drag_line = vis.append("line")
-    .attr("class", "drag_line")
-    .attr("x1", 0)
-    .attr("y1", 0)
-    .attr("x2", 0)
-    .attr("y2", 0);
+  // line displayed when dragging new nodes
+  var drag_line = vis.append("line")
+                  .attr("class", "drag_line")
+                  .attr("x1", 0)
+                  .attr("y1", 0)
+                  .attr("x2", 0)
+                  .attr("y2", 0);
 
-// get layout properties
-var nodes = force.nodes(),
-    links = force.links(),
-    node = vis.selectAll(".node"),
-    link = vis.selectAll(".link");
+  // get layout properties
+  var nodes = force.nodes(),
+      links = force.links(),
+      node = vis.selectAll(".node"),
+      link = vis.selectAll(".link");
 
-// add keyboard callback
-d3.select(window)
-    .on("keydown", keydown);
+  // add keyboard callback
+  d3.select(window)
+  .on("keydown", keydown);
 
-redraw();
+  redraw();
 
-// focus on svg
-// vis.node().focus();
+  // focus on svg
+  // vis.node().focus();
 
-function mousedown() {
-  if (!mousedown_node && !mousedown_link) {
-    // allow panning if nothing is selected
-    vis.call(d3.behavior.zoom().on("zoom"), rescale);
-    return;
+  function mousedown() {
+    if (!mousedown_node && !mousedown_link) {
+      // allow panning if nothing is selected
+      vis.call(d3.behavior.zoom().on("zoom"), rescale);
+      return;
+    }
   }
-}
 
-function mousemove() {
-  if (!mousedown_node) return;
+  function mousemove() {
+    if (!mousedown_node) return;
 
-  // update drag line
-  drag_line
-      .attr("x1", mousedown_node.x)
-      .attr("y1", mousedown_node.y)
-      .attr("x2", d3.svg.mouse(this)[0])
-      .attr("y2", d3.svg.mouse(this)[1]);
-
-}
-
-function mouseup() {
-  if (mousedown_node) {
-    // hide drag line
+    // update drag line
     drag_line
+    .attr("x1", mousedown_node.x)
+    .attr("y1", mousedown_node.y)
+    .attr("x2", d3.svg.mouse(this)[0])
+    .attr("y2", d3.svg.mouse(this)[1]);
+
+  }
+
+  function mouseup() {
+    if (mousedown_node) {
+      // hide drag line
+      drag_line
       .attr("class", "drag_line_hidden")
 
-    if (!mouseup_node) {
-      // add node
-      var point = d3.mouse(this),
-        node = {x: point[0], y: point[1]},
-        n = nodes.push(node);
+      if (!mouseup_node) {
+        // add node
+        var point = d3.mouse(this),
+            node = {x: point[0], y: point[1]},
+            n = nodes.push(node);
 
-      // select new node
-      selected_node = node;
-      selected_link = null;
-      
-      // add link to mousedown node
-      links.push({source: mousedown_node, target: node});
+        // select new node
+        selected_node = node;
+        selected_link = null;
+        
+        // add link to mousedown node
+        links.push({source: mousedown_node, target: node});
+      }
+
+      redraw();
     }
-
-    redraw();
+    // clear mouse event vars
+    resetMouseVars();
   }
-  // clear mouse event vars
-  resetMouseVars();
-}
 
-function resetMouseVars() {
-  mousedown_node = null;
-  mouseup_node = null;
-  mousedown_link = null;
-}
+  function resetMouseVars() {
+    mousedown_node = null;
+    mouseup_node = null;
+    mousedown_link = null;
+  }
 
-function tick() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+  function tick() {
+    link.attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; });
 
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
-}
+    node.attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; });
+  }
 
-// rescale g
-function rescale() {
-  trans=d3.event.translate;
-  scale=d3.event.scale;
+  // rescale g
+  function rescale() {
+    trans=d3.event.translate;
+    scale=d3.event.scale;
 
-  vis.attr("transform",
-      "translate(" + trans + ")"
-      + " scale(" + scale + ")");
-}
+    vis.attr("transform",
+             "translate(" + trans + ")"
+                         + " scale(" + scale + ")");
+  }
 
-// redraw force layout
-function redraw() {
+  // redraw force layout
+  function redraw() {
 
-  link = link.data(links);
+    link = link.data(links);
 
-  link.enter().insert("line", ".node")
-      .attr("class", "link")
-      .on("mousedown", 
+    link.enter().insert("line", ".node")
+    .attr("class", "link")
+    .on("mousedown", 
         function(d) { 
           mousedown_link = d; 
           if (mousedown_link == selected_link) selected_link = null;
@@ -151,17 +152,17 @@ function redraw() {
           redraw(); 
         })
 
-  link.exit().remove();
+    link.exit().remove();
 
-  link
+    link
     .classed("link_selected", function(d) { return d === selected_link; });
 
-  node = node.data(nodes);
+    node = node.data(nodes);
 
-  node.enter().insert("circle")
-      .attr("class", "node")
-      .attr("r", 5)
-      .on("mousedown", 
+    node.enter().insert("circle")
+    .attr("class", "node")
+    .attr("r", 5)
+    .on("mousedown", 
         function(d) { 
           // disable zoom
           vis.call(d3.behavior.zoom().on("zoom"), null);
@@ -173,19 +174,19 @@ function redraw() {
 
           // reposition drag line
           drag_line
-              .attr("class", "link")
-              .attr("x1", mousedown_node.x)
-              .attr("y1", mousedown_node.y)
-              .attr("x2", mousedown_node.x)
-              .attr("y2", mousedown_node.y);
+          .attr("class", "link")
+          .attr("x1", mousedown_node.x)
+          .attr("y1", mousedown_node.y)
+          .attr("x2", mousedown_node.x)
+          .attr("y2", mousedown_node.y);
 
           redraw(); 
         })
-      .on("mousedrag",
+    .on("mousedrag",
         function(d) {
           // redraw();
         })
-      .on("mouseup", 
+    .on("mouseup", 
         function(d) { 
           if (mousedown_node) {
             mouseup_node = d; 
@@ -205,53 +206,56 @@ function redraw() {
           } 
         })
     .transition()
-      .duration(750)
-      .ease("elastic")
-      .attr("r", 6.5);
+    .duration(750)
+    .ease("elastic")
+    .attr("r", 6.5);
 
-  node.exit().transition()
-      .attr("r", 0)
+    node.exit().transition()
+    .attr("r", 0)
     .remove();
 
-  node
+    node
     .classed("node_selected", function(d) { return d === selected_node; });
 
-  
+    
 
-  if (d3.event) {
-    // prevent browser's default behavior
-    d3.event.preventDefault();
+    if (d3.event) {
+      // prevent browser's default behavior
+      d3.event.preventDefault();
+    }
+
+    force.start();
+
   }
 
-  force.start();
+  function spliceLinksForNode(node) {
+    toSplice = links.filter(
+      function(l) { 
+        return (l.source === node) || (l.target === node); });
+    toSplice.map(
+      function(l) {
+        links.splice(links.indexOf(l), 1); });
+  }
 
-}
-
-function spliceLinksForNode(node) {
-  toSplice = links.filter(
-    function(l) { 
-      return (l.source === node) || (l.target === node); });
-  toSplice.map(
-    function(l) {
-      links.splice(links.indexOf(l), 1); });
-}
-
-function keydown() {
-  if (!selected_node && !selected_link) return;
-  switch (d3.event.keyCode) {
-    case 8: // backspace
-    case 46: { // delete
-      if (selected_node) {
-        nodes.splice(nodes.indexOf(selected_node), 1);
-        spliceLinksForNode(selected_node);
+  function keydown() {
+    if (!selected_node && !selected_link) return;
+    switch (d3.event.keyCode) {
+      case 8: // backspace
+      case 46: { // delete
+        if (selected_node) {
+          nodes.splice(nodes.indexOf(selected_node), 1);
+          spliceLinksForNode(selected_node);
+        }
+        else if (selected_link) {
+          links.splice(links.indexOf(selected_link), 1);
+        }
+        selected_link = null;
+        selected_node = null;
+        redraw();
+        break;
       }
-      else if (selected_link) {
-        links.splice(links.indexOf(selected_link), 1);
-      }
-      selected_link = null;
-      selected_node = null;
-      redraw();
-      break;
     }
   }
 }
+
+force_view_js();
--- shahir/prs/js/mn.js
+++ shahir/prs/js/mn.js
@@ -1,81 +1,84 @@
 
- var mpnet = JSON.parse('{"layers": [{"name": 0}, {"name": 1}, {"name": 2}], "nodes": [{"name": 0}, {"name": 1}, {"name": 2}, {"name": 3}, {"name": 4}, {"name": 5}, {"name": 6}, {"name": 7}, {"name": 8}, {"name": 9}], "links": [{"source": 1, "layer": 0, "target": 6, "value": 1}, {"source": 2, "layer": 0, "target": 5, "value": 1}, {"source": 2, "layer": 0, "target": 3, "value": 1}, {"source": 3, "layer": 0, "target": 5, "value": 1}, {"source": 5, "layer": 0, "target": 6, "value": 1}, {"source": 7, "layer": 0, "target": 9, "value": 1}, {"source": 8, "layer": 0, "target": 9, "value": 1}, {"source": 0, "layer": 1, "target": 6, "value": 1}, {"source": 0, "layer": 1, "target": 1, "value": 1}, {"source": 1, "layer": 1, "target": 8, "value": 1}, {"source": 1, "layer": 1, "target": 3, "value": 1}, {"source": 1, "layer": 1, "target": 9, "value": 1}, {"source": 1, "layer": 1, "target": 4, "value": 1}, {"source": 1, "layer": 1, "target": 6, "value": 1}, {"source": 2, "layer": 1, "target": 5, "value": 1}, {"source": 3, "layer": 1, "target": 5, "value": 1}, {"source": 6, "layer": 1, "target": 7, "value": 1}, {"source": 6, "layer": 1, "target": 8, "value": 1}, {"source": 7, "layer": 1, "target": 9, "value": 1}, {"source": 0, "layer": 2, "target": 3, "value": 1}, {"source": 0, "layer": 2, "target": 6, "value": 1}, {"source": 1, "layer": 2, "target": 7, "value": 1}, {"source": 1, "layer": 2, "target": 2, "value": 1}, {"source": 1, "layer": 2, "target": 5, "value": 1}, {"source": 2, "layer": 2, "target": 5, "value": 1}, {"source": 3, "layer": 2, "target": 7, "value": 1}, {"source": 3, "layer": 2, "target": 5, "value": 1}, {"source": 5, "layer": 2, "target": 9, "value": 1}, {"source": 8, "layer": 2, "target": 9, "value": 1}]}');
+var mn_js = function () {
+  var mpnet = JSON.parse('{"layers": [{"name": 0}, {"name": 1}, {"name": 2}], "nodes": [{"name": 0}, {"name": 1}, {"name": 2}, {"name": 3}, {"name": 4}, {"name": 5}, {"name": 6}, {"name": 7}, {"name": 8}, {"name": 9}], "links": [{"source": 1, "layer": 0, "target": 6, "value": 1}, {"source": 2, "layer": 0, "target": 5, "value": 1}, {"source": 2, "layer": 0, "target": 3, "value": 1}, {"source": 3, "layer": 0, "target": 5, "value": 1}, {"source": 5, "layer": 0, "target": 6, "value": 1}, {"source": 7, "layer": 0, "target": 9, "value": 1}, {"source": 8, "layer": 0, "target": 9, "value": 1}, {"source": 0, "layer": 1, "target": 6, "value": 1}, {"source": 0, "layer": 1, "target": 1, "value": 1}, {"source": 1, "layer": 1, "target": 8, "value": 1}, {"source": 1, "layer": 1, "target": 3, "value": 1}, {"source": 1, "layer": 1, "target": 9, "value": 1}, {"source": 1, "layer": 1, "target": 4, "value": 1}, {"source": 1, "layer": 1, "target": 6, "value": 1}, {"source": 2, "layer": 1, "target": 5, "value": 1}, {"source": 3, "layer": 1, "target": 5, "value": 1}, {"source": 6, "layer": 1, "target": 7, "value": 1}, {"source": 6, "layer": 1, "target": 8, "value": 1}, {"source": 7, "layer": 1, "target": 9, "value": 1}, {"source": 0, "layer": 2, "target": 3, "value": 1}, {"source": 0, "layer": 2, "target": 6, "value": 1}, {"source": 1, "layer": 2, "target": 7, "value": 1}, {"source": 1, "layer": 2, "target": 2, "value": 1}, {"source": 1, "layer": 2, "target": 5, "value": 1}, {"source": 2, "layer": 2, "target": 5, "value": 1}, {"source": 3, "layer": 2, "target": 7, "value": 1}, {"source": 3, "layer": 2, "target": 5, "value": 1}, {"source": 5, "layer": 2, "target": 9, "value": 1}, {"source": 8, "layer": 2, "target": 9, "value": 1}]}');
 
- var color = d3.scale.category20();
- var svg_layer=[];
- var node_layer=[];
- var link_layer=[];
- var layer_label=[];
+  var color = d3.scale.category20();
+  var svg_layer=[];
+  var node_layer=[];
+  var link_layer=[];
+  var layer_label=[];
 
- // Calculate size for the figure
- var width = Math.sqrt(mpnet.nodes.length)*70;//500;
- var height = 4/6*width;
- var fontsize = Math.max(width*0.05,16)
+  // Calculate size for the figure
+  var width = Math.sqrt(mpnet.nodes.length)*70;//500;
+  var height = 4/6*width;
+  var fontsize = Math.max(width*0.05,16)
 
- var force = d3.layout.force()
-                      .charge(-120)
-                      .linkDistance(30)
-                      .size([width, height])
-                      .nodes(mpnet.nodes)
-                      .links(mpnet.links)
-                      .start();
+  var force = d3.layout.force()
+              .charge(-120)
+              .linkDistance(30)
+              .size([width, height])
+              .nodes(mpnet.nodes)
+              .links(mpnet.links)
+              .start();
 
- var nlayers=mpnet.layers.length;
+  var nlayers=mpnet.layers.length;
 
- for (var layer=nlayers-1;layer>=0;layer--){
-  svg_layer[layer] = d3.select("#chart2").append("svg")
-                     .attr("layer",0)
-                     .style("position","absolute")
-                     .style("left","-250px")
-                     .style("top",(width/6+layer*width/4+25).toString()+"px")
-                     .style("background-color","rgba(100,100,100,0.3)")
-                     .style("transform","rotate3D(-0.9,0.4,0.4,70deg)") // Firefox
-                     .style("-webkit-transform","rotate3D(0.6,0.4,.5,70deg)") // Safari, Chrome 
-                     .attr("width", width)
-                     .attr("height", height);
+  for (var layer=nlayers-1;layer>=0;layer--){
+    svg_layer[layer] = d3.select("#chart2").append("svg")
+                       .attr("layer",0)
+                       .style("position","absolute")
+                       .style("left","-250px")
+                       .style("top",(width/6+layer*width/4+25).toString()+"px")
+                       .style("background-color","rgba(100,100,100,0.3)")
+                       .style("transform","rotate3D(-0.9,0.4,0.4,70deg)") // Firefox
+                       .style("-webkit-transform","rotate3D(0.6,0.4,.5,70deg)") // Safari, Chrome 
+                       .attr("width", width)
+                       .attr("height", height);
 
-  layer_label[layer]=svg_layer[layer].selectAll(".layerlabel")
-                     .data([mpnet.layers[layer]])
-                     .enter()
-                     .append("text")
-                     .text(function(d){return d.name;})
-                     .attr("dx",function(d){return width-0.8*d.name.toString().length*fontsize;})
-                     .attr("dy",fontsize)
-                     .style("font-size",fontsize+"px")
-                     .style("fontcolor","black")
+    layer_label[layer]=svg_layer[layer].selectAll(".layerlabel")
+                       .data([mpnet.layers[layer]])
+                       .enter()
+                       .append("text")
+                       .text(function(d){return d.name;})
+                       .attr("dx",function(d){return width-0.8*d.name.toString().length*fontsize;})
+                       .attr("dy",fontsize)
+                       .style("font-size",fontsize+"px")
+                       .style("fontcolor","black")
 
-  link_layer[layer] = svg_layer[layer].selectAll(".link")
-                      .data(mpnet.links)
-                      .enter()
-                      .append("line")
-                      .filter(function(d){return d.layer==layer})
-                      .attr("class", "link")
-                      .style("stroke-width", function(d) { return 2*Math.sqrt(d.value); })
-                      .style("stroke","#999");
+    link_layer[layer] = svg_layer[layer].selectAll(".link")
+                        .data(mpnet.links)
+                        .enter()
+                        .append("line")
+                        .filter(function(d){return d.layer==layer})
+                        .attr("class", "link")
+                        .style("stroke-width", function(d) { return 2*Math.sqrt(d.value); })
+                        .style("stroke","#999");
 
-  node_layer[layer] = svg_layer[layer].selectAll(".node")
-                      .data(mpnet.nodes)
-                      .enter().append("circle")
-                      .attr("class", "node")
-                      .attr("r", 5)
-                      .style("fill", function(d) {return color(d.index); })
-                      .style("stroke","#fff")
-                      .style("stroke-width","1.5px")
-                      .call(force.drag);
+    node_layer[layer] = svg_layer[layer].selectAll(".node")
+                        .data(mpnet.nodes)
+                        .enter().append("circle")
+                        .attr("class", "node")
+                        .attr("r", 5)
+                        .style("fill", function(d) {return color(d.index); })
+                        .style("stroke","#fff")
+                        .style("stroke-width","1.5px")
+                        .call(force.drag);
 
-  node_layer[layer].append("title")
-                   .text(function(d) { return d.name; });
- }
-
- force.on("tick", function() {
-  for (var layer=0;layer<nlayers;layer++){
-   link_layer[layer].attr("x1", function(d) { return d.source.x; })
-                    .attr("y1", function(d) { return d.source.y; })
-                    .attr("x2", function(d) { return d.target.x; })
-                    .attr("y2", function(d) { return d.target.y; });
-
-   node_layer[layer].attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; });
+    node_layer[layer].append("title")
+    .text(function(d) { return d.name; });
   }
- });
 
+  force.on("tick", function() {
+    for (var layer=0;layer<nlayers;layer++){
+      link_layer[layer].attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+      node_layer[layer].attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+    }
+  });
+};
+
+mn_js();
